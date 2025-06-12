ba690506fc78cc2bde4334434bef2454a74b29ba
Fabricio Montalvo <fabri032004@gmail.com>
2025-06-11 20:48:35 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
-
-db.connect((err) => {
-  if (err) {
-    console.error('❌ Error al conectar a la base de datos:', err);
-  } else {
-    console.log('✅ Conexión exitosa a la base de datos.');
-  }
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = db;
